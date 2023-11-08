373884c8c961d6d72513a850bfe743757c0907ff
MichalRzeszot <134526382+MichalRzeszot@users.noreply.github.com>
2023-11-08 19:46:00 +0100

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const formData = new FormData(loginForm);
+  const formObject = {};
+
+  formData.forEach((value, key) => {
+    formObject[key] = value;
+  });
+
+  if (formObject.email && formObject.password) {
+    console.log(formObject);
+
+    loginForm.reset();
+  } else {
+    alert("Wszystkie pola powinny być wypełnione");
+  }
+});
